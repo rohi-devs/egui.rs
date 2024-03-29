c955c04366d2b9c24a8cf23438dd4abf13bfa2b5
Rohith <rohith.dev@zohomail.in>
2024-03-30 00:26:01 +0530

    async call hack

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,7 @@ use eframe::{egui, App};
 
 fn main() -> Result<(), eframe::Error> {
 
-    let opt = eframe::NativeOptions {
-        viewport : egui::ViewportBuilder::default().with_inner_size([1920.0, 1080.0]),
-        ..Default::default()
-    };
+    let opt = eframe::NativeOptions::default();
 
     eframe::run_native("ROHITH TRAIL",opt, Box::new(|cc|{
         egui_extras::install_image_loaders(&cc.egui_ctx);
@@ -22,6 +19,8 @@ fn main() -> Result<(), eframe::Error> {
 struct MyApp {
     name : String,
     age : i32,
+    url : String,
+    response : Option<String>
 }
 
 impl Default for MyApp {
@@ -29,6 +28,8 @@ impl Default for MyApp {
         Self {
             name : "Rohith".to_string(),
             age : 20,
+            url : "https://www.google.com".to_string(),
+            response : None
         }
     }
     
@@ -39,10 +40,30 @@ impl App for MyApp{
         egui::CentralPanel::default().show(ctx, |ui| {
             ui.heading("Hello World");
             ui.label("Name");
-            // ui.text_edit_singleline(&mut self.name);
+            ui.text_edit_multiline(&mut self.url);
+            let _r = ui.button("Click me");
+            if _r.clicked() {
+                if let url = &self.url {
+                    let req = reqwest::get(url);
+                    let _rt = tokio::runtime::Runtime::new().unwrap();
+                    let _res = _rt.block_on(req).unwrap();
+                    let res = _rt.block_on(_res.text()).unwrap();
+                    self.response = Some(res);
+                }
+            }
             ui.text_edit_multiline(&mut self.name);
             ui.label("Age");
             ui.add(egui::Slider::new(&mut self.age, 0..=100));
+            if let Some(mut res) = Some(self.response.clone()) {
+                match res {
+                    Some(mut r) => {
+                        ui.label("Response");
+                        ui.text_edit_multiline(&mut r);
+                    },
+                    None => {}
+                    
+                }
+            }
         });
     }
 }
